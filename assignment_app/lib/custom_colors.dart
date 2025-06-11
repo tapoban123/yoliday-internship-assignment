2fb8b22ab88148d009c0b0da317982a97f726deb
Tapoban Ray <raytapoban@gmail.com>
2025-06-11 19:14:59 +0530

    Initial SetUp

---
+++ assignment_app/lib/custom_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  CustomColors._();
+
+  Color deepOrangeColor = const Color(0xFFDF5532);
+  Color orangeColor = const Color(0xFFF39519);
+  Color yellowColor = const Color(0xFFFFCD67);
+  Color textColor = const Color(0xFF303030);
+  Color textGreyColor = const Color(0xFF9E95A2);
+  Color greyColor = const Color(0xFFD6D1D5);
+}
